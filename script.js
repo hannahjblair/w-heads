52db45c63b0a359f6244ea5a1c1f6d472bc6baad
Hannah Blair <hannahjblair@protonmail.com>
2023-11-21 00:15:13 -0500

    added an array of 5 objects that each represent a player

---
+++ script.js
@@ -0,0 +1,42 @@
+
+let players = [
+    {
+        "name": "Stewie",
+        "pts": 23.0,
+        "reb": 9.3,
+        "blk": 1.6,
+        "pic": "<img src=stewie.png>"
+    },
+
+    {
+        "name": "Arike",
+        "pts": 21.2,
+        "reb": 3.4,
+        "blk": .1,
+        "pic": "<img src=arike.png>"
+    },
+
+    {
+        "name": "BG",
+        "pts": 17.7,
+        "reb": 6.3,
+        "blk": 1.6,
+        "pic": "<img src=BG.png>"
+    },
+
+    {
+        "name": "Chelsea Grey",
+        "pts": 15.3,
+        "reb": 4.0,
+        "blk": .6,
+        "pic": "<img src=chelseagrey.png>"
+    },
+
+    {
+        "name": "Jewell",
+        "pts": 35.4,
+        "reb": 4.7,
+        "blk": .4,
+        "pic": "<img src=jewell.png>"
+    }
+]
